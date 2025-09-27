a31ab9eea1b6330d079e8cf4a90910c8c8b748cb
BB <biring@gmail.com>
2025-09-27 17:42:29 -0400

    test(main): integrate sample test into runner
    - forward declare test_always_pass in test_main.c
    
    - register test_always_pass with RUN_TEST for execution

--- test/test_main.c
+++ test/test_main.c
@@ -1,8 +1,12 @@
 #include "unity.h"
 
+// Forward declarations
+void test_always_pass(void);
+
 int main(void) {
     UNITY_BEGIN();
-
+    
+    RUN_TEST(test_always_pass);
+    
     return UNITY_END();
 }
-
